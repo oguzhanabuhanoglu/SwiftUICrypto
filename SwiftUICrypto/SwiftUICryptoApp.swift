7d3d1c084d6f78c1cefecd68fbad7d7fc1201aba
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-05-10 19:44:45 +0300

    firs commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Oğuzhan Abuhanoğlu on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
