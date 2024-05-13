05362346d25e71177983741b182be27592db4e61
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-05-13 16:13:52 +0300

    Color extension added

---
+++ SwiftUICrypto/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  SwiftUICrypto
+//
+//  Created by Oğuzhan Abuhanoğlu on 13.05.2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
